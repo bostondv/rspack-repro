3b43e54de7beb12ab80ab6c6546ee801140c3ac6
Boston Dell-Vandenberg <bostondv@gmail.com>
2024-03-20 10:50:34 -0400

    swc plugin

--- config.mjs
+++ config.mjs
@@ -1,8 +1,6 @@
 import path from "path";
 import { fileURLToPath } from "url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import rspack from '@rspack/core'
-import ReactRefreshPlugin from '@rspack/plugin-react-refresh'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const isRunningWebpack = !!process.env.WEBPACK;
@@ -16,38 +14,17 @@ if (!isRunningRspack && !isRunningWebpack) {
  */
 const config = {
   mode: "development",
-  devtool: 'eval-cheap-module-source-map',
+  devtool: false,
   entry: {
     main: "./src/index",
   },
-  plugins: [
-    new HtmlWebpackPlugin(),
-    new rspack.HotModuleReplacementPlugin(),
-    new ReactRefreshPlugin(),
-  ],
-  optimization: {
-    minimize: false,
-    mergeDuplicateChunks: false,
-    moduleIds: 'deterministic',
-    chunkIds: 'deterministic',
-    splitChunks: {
-      chunks: 'all',
-      maxAsyncRequests: 5,
-      maxInitialRequests: 5,
-      minChunks: 1,
-      minSize: 1,
-      name: false,
-    },
-  },
+  plugins: [new HtmlWebpackPlugin()],
   output: {
     clean: true,
     path: isRunningWebpack
       ? path.resolve(__dirname, "webpack-dist")
       : path.resolve(__dirname, "rspack-dist"),
-    // filename: "[name].js",
-    // chunkFilename: '[name].chunk.js',
-    filename: '[name].webpack_bundle-[contenthash].js',
-    chunkFilename: '[name]-[contenthash].webpack_chunk.js',
+    filename: "[name].js",
   },
   experiments: {
     css: true,
@@ -55,50 +32,25 @@ const config = {
   module: {
     rules: [
       {
-        test: /(\.(t|j)sx?$)/,
+        test: /\.js$/,
         exclude: [/node_modules/],
         include: [path.resolve(__dirname, "src")],
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              cacheDirectory: false,
-              cacheCompression: false,
-              presets: [
-                [
-                  '@babel/preset-env',
-                  {
-                    useBuiltIns: 'entry',
-                    corejs: '3.27',
-                    modules: false,
-                    include: ['transform-classes'],
-                  },
+        use: {
+          loader: 'builtin:swc-loader',
+          options: {
+            jsc: {
+              parser: {
+                syntax: 'ecmascript',
+              },
+              experimental: {
+                plugins: [
+                  ['@swc/plugin-remove-console', { exclude: 'error' }],
                 ],
-                ['@babel/react', { runtime: 'automatic', importSource: '@emotion/react' }],
-                '@babel/preset-typescript',
-              ],
-              plugins: [
-                '@babel/plugin-syntax-dynamic-import',
-                'graphql-tag',
-                ['@babel/plugin-proposal-decorators', { legacy: true }],
-                [
-                  '@emotion',
-                  {
-                    sourceMap: true,
-                    autoLabel: 'never',
-                    labelFormat: '',
-                    cssPropOptimization: true,
-                  },
-                ],
-                'react-refresh/babel'
-              ],
-              assumptions: {
-                privateFieldsAsProperties: true,
-                setPublicClassFields: true,
               },
             },
           },
-        ],
+        },
+        type: 'javascript/auto',
       },
     ]
   },
--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import { createRoot } from 'react-dom/client'
-import React from 'react'
-import App from './App'
-
-const el = document.createElement('div')
-document.getElementsByTagName('body')[0].appendChild(el)
-
-const root = createRoot(el)
-
-root.render(<App />)
+import('./render').then(exports => {
+  exports.render()
+})
